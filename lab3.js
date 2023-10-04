ecac24c932e84d8b71126029948ea306c6142583
Viska19 <116948900+Viska19@users.noreply.github.com>
2023-10-04 16:10:46 +0300

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -1,105 +1,48 @@
-"use strict";
-
-// возвращает n-е число Фибоначчи
-import {fib}  from '../lab2/fib.js';
-
-/**
- * возвращает дробную часть числа num
- * @param {number} num - возводимое число в степень
- * @return {number} - возвращает дробную часть
- */
+import {
+    fib
+} from './lab2.js';
 export function getDecimal(num) {
-  let w = Math.abs(num - Math.floor(num));
-  return parseFloat(w.toFixed(2));
+    if (Math.trunc(num) == num) {
+        return 0;
+    }
+    else if (num < 0) {
+        return num - (-Math.ceil(-num));
+    }
+    if (num > 1) {
+        return +(num - Math.trunc(num)).toFixed(2);
+    }
 }
-
-
-/**
- * возвращает строку str с заглавным первым символом
- * @param {string} str - входная строка
- * @return {string} - Строка с первой буквой, написанной с заглавной буквы
- */
-export function ucFirst(str) {
-  if (!str) {
-    return str;
-  } else {
-    return str[0].toUpperCase() + str.slice(1);
-  }
+export function ucFirst(str){
+    if (!str) return str;
+        return str[0].toUpperCase() + str.slice(1);
 }
-
-
-/** 
-* возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false
-* @param {string} str - Строка для проверки.
-* @returns {boolean} - Возвращает true, если строка содержит слова "viagra" или "xxx", иначе - false.
-*/
-export function checkSpam(str) {
-  let lowerStr = str.toLowerCase();
-  
+export function checkSpam(str){
+    let lowerStr = str.toLowerCase();
   return lowerStr.includes('viagra') || lowerStr.includes('xxx');
 }
-
-/**
- * проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
-  * @param {string} str - строка, которую нужно обрезать
-  * @param {number} maxlength - максимальная длина строки
-  * @return {string} - обрезанная строка или исходная строка, если ее длина не превышает максимальную
- */
-export function truncate(str, maxlength) {
-  if (str.length > maxlength) {
-    return str.slice(0, maxlength - 1) + '…';
-  } else {
-    return str;
-  }
+export function  truncate(str, maxlength){
+    return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
 }
-
-
-/**
- * преобразует строку вида 'var-test-text' в 'varTestText'
- * @param {string} str - Строка для преобразования
- * @returns {string} - соединяет [var test text] в 'varTestText'
- */export function camelize(str) {
-  let words = str.split('-');
-  
-  for (let i = 1; i < words.length; i++) {
-    words[i] = ucFirst(words[i]);
-  }
-  
-  return words.join('');
+export function  camelize(str){
+    let STR = str.split("-");
+    let StrNew = "";
+    for (let i = 0; i <= STR.length - 1; i++) {
+        if (i == 0) StrNew = STR[0];
+        else StrNew += ucFirst(STR[i]);
+    }
+    return StrNew;
 }
-
-
-
-
-
-/**
- * возвращает n-е число Фибоначчи
- * @param {number} n - число Фибоначчи, до которого необходимо всё вычислить
- * @return {array} - Массив, содержащий последовательность Фибоначчи с точностью до n
- */
-export function fibs(n) {
-  const arr = [];
-  for (let i = 0; i < n; i++) {
-    arr.push(fib(i));
-  }
-  return arr;
+export function  fibs(n){
+    let arr = [];
+    for (let i = 0; i < n; i++) arr.push(fib(i));
+    return arr;
 }
-/**
- * принимает неупорядоченный массив чисел arr и возвращает массив из тех же элементов
- * @param {array} arr - Исходный массив.
- * @returns {array} sortedArr - Отсортированный массив.
- */
-export function arrReverseSorted(arr) {
-  const sortedArr = arr.slice().sort((a, b) => b - a);
-  
-  return sortedArr;
+export function  arrReverseSorted(arr){
+    return arr.sort(function (a, b) {
+        return b - a;
+    })
 }
-
-/**
- * вернёт массив уникальных, не повторяющихся значений массива arr
- * @param {array} arr - массив, который нужно обработать
- * @returns {array} - массив с уникальными значениями
- */
-export function unique(arr) {
-  return Array.from(new Set(arr));
+export function  unique(arr){
+    return Array.from(new Set(arr));
 }
